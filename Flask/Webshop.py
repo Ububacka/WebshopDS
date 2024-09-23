f19170b5bf186fb0024e2f7a58072f6ae8bc90e6
Brotsocke <Brotsocke69xlo@gmx.de>
2024-09-23 16:48:37 +0200

    HA

--- Flask/Webshop.py
+++ Flask/Webshop.py
@@ -19,7 +19,7 @@ def sky():
 @app.route('/search')  # 'GET' is the default method, you don't need to mention it explicitly
 def search():
 
-
+    # query = request.args['search']
     query = request.GET.get('search')  # try this instead
 
     req_search = Storage.query.filter_by(req_no=query)
@@ -50,12 +50,10 @@ def Tic():
 '''
 
 
-
-
-
 @app.route("/skybase/Top-Items")
 def Top_Items():
     return render_template("Kategorie.html")
+
 
 @app.route("/skybase/Charts")
 def Charts():
@@ -68,10 +66,6 @@ def Warenkorb():
 @app.route("/skybase/Impressum")
 def Impressum():
     return render_template("Impressum.html")
-
-
-
-
 
 
 if __name__ == '__main__':
